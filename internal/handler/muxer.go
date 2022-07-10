bb1044ecf03609a1b0ab2e2c1bce662ff1b52ee3
Ilya Sazonov <isaznn@yandex.ru>
2022-07-10 19:06:59 +0300

    Describe data structures

--- internal/handler/muxer.go
+++ internal/handler/muxer.go
@@ -1,9 +1,23 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
+
+type muxerHandlerRequest struct {
+	Urls []string `json:"urls"`
+}
+
+type urlWithResponse struct {
+	Url      string          `json:"url"`
+	Response json.RawMessage `json:"response"`
+}
+
+type muxerHandlerResponse struct {
+	Result []urlWithResponse `json:"result"`
+}
 
 func (h *Handler) MuxerHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
